f1a9912f3103abf814f69a2319b4a8ad7aa63576
DiosTeOdia <art3611@gmail.com>
2024-05-01 14:27:59 -0600

    configuracion de tailwindcss

---
+++ App/public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/main.css">
+  <title>Aplicacion</title>
+</head>
+<body class="bg-black">
+  <h1 class="text-4xl text-red-500">Aplicacion</h1>
+</body>
+</html>

0878f0329e36d444823d324e3ae555a0510700d1
DiosTeOdia <art3611@gmail.com>
2024-05-01 14:38:56 -0600

    Agrego: configuracion del servidor de desarrollo

--- App/public/index.php
+++ App/public/index.php
@@ -4,9 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./assets/css/main.css">
-  <title>Aplicacion</title>
+  <title>Bolsa de trabajo</title>
 </head>
 <body class="bg-black">
-  <h1 class="text-4xl text-red-500">Aplicacion</h1>
+  <h1 class="text-4xl text-white">Hola Mundo</h1>
 </body>
 </html>

daa160a32950d8472c3f5dccbee21beb39a712ce
DiosTeOdia <art3611@gmail.com>
2024-05-09 21:38:47 -0600

    estilos como empiezo

--- bolsa-trabajo/views/inicio/index.php
+++ bolsa-trabajo/views/inicio/index.php
@@ -1,3 +1,30 @@
 <?php require 'views/partials/header.php'?>
-    <h1>Este es el inicio</h1>
+<main class='flex flex-col'>
+    <section>
+        <h1 class='text-5xl text-center'>Trabajos recomendados</h1>
+        <p class='text-center'>Podemos ayudarte a encontrar tu trabako deseado</p>
+    </section>
+    
+    <section>
+        <h2 class="text-center text-3xl">Como empiezo</h2>
+        <article class='flex justify-center gap-20 mt-10'>
+        <div>
+            <h3 class='text-center'>Registrate</h3>
+            <p class='text-gray'>Registrate en nuestra plataforma</p>
+
+        </div>
+        <div>
+            <h3 class='text-center'>Crea tu perfil</h3>
+            <p class='text-gray'>Agrega tus datos personales y de contacto</p>
+        </div>
+        <div>
+            <h3 class='text-center'>Aplica a un trabajo</h3>
+             <p class='text-gray'>Busca tu trabajo deseado</p>
+        </div>
+        </article>
+                
+    </section>
+
+    
+</main> 
 <?php require 'views/partials/footer.php'?>

1040acbb6b32f96abf5b5122d86824671690464d
DiosTeOdia <art3611@gmail.com>
2024-05-11 19:30:44 -0600

    agrego footer

--- bolsa-trabajo/views/partials/footer.php
+++ bolsa-trabajo/views/partials/footer.php
@@ -1,2 +1,29 @@
+<footer class='flex flex-col align-center justify-center'> 
+    <div class='flex justify-between items-center border-b-1 border-b p-10'>
+        <div>
+          <img src="<?= constant('URL')?>public/img/logo.png" alt="Imagen de la empresa">
+        </div>
+      
+        <div>
+          <h6 class='font-bold text-xl'>Para candidatos</h6>
+          <ul class='text-slate-500 text-xl space-y-5 mt-2'>
+              <li>Buscar trabajo</li>
+              <li>Aplicar a un trabajo</li>
+              <li>Registrarme</li>
+          </ul>
+        </div>
+        <div>
+          <h6 class='font-bold text-xl'>Para empleados</h6>
+          <ul class='text-slate-500 text-xl space-y-5 mt-2'>
+              <li>Publicar una oferta</li>
+              <li>Buscar candidatos</li>
+              <li>Registrar mi empresa</li>
+          </ul>
+        </div>
+    </div>
+    <div class='p-10'>
+            <p class='text-center'>@2024 Todos los derechos reservados</p>
+     </div>
+</footer>
 </body>
 </html>

99a1027c68cfe9d1f4bcf5a7e5f007186e6d65c7
DiosTeOdia <art3611@gmail.com>
2024-05-14 17:01:54 -0600

    agrego rutas al navbar

--- bolsa-trabajo/views/partials/navbar.php
+++ bolsa-trabajo/views/partials/navbar.php
@@ -1,14 +1,18 @@
 <header>
-    <nav class='flex justify-between px-20 py-5 border border-b-2 '>
+    <nav class='flex justify-between items-center px-20 py-5 border border-b-2 '>
         <div class='flex gap-10 '>
             <a href="<?= constant('URL')?>/">Inicio</a>
             <a href="<?= constant('URL')?>trabajos/">Trabajos</a>
             <a href="<?= constant('URL')?>empresas/">Empresas</a>
         </div>
-        <div class="flex gap-x-5">
-            <a href="<?= constant('URL')?>login/">Iniciar Sesion</a>
-            <p>/</p>
-            <a href="<?= constant('URL')?>registro/">Registrarse</a>
+
+        <div class='flex gap-x-5'>
+            <div class="flex gap-x-5 bg-[#DCE8F8] p-5 text-[#2A56CB] rounded-lg">
+                <a href="<?= constant('URL')?>login/">Iniciar Sesion</a> 
+            </div>
+            <div class="flex gap-x-5 bg-[#DCE8F8] p-5 text-[#2A56CB] rounded-lg">
+              <a href="<?= constant('URL')?>registro/">Registrarme</a>
+            </div>
         </div>
     </nav>
 </header>

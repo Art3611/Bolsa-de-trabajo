0fdbbb648ecfdb61443a30077025a45343abbc4c
DiosTeOdia <art3611@gmail.com>
2024-05-19 14:50:36 -0600

    agrego perfil usuario

---
+++ bolsa-trabajo/controllers/perfilUsuario.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/userSesion.php';
+
+class PerfilUsuario extends Controller {
+
+    public $view;
+    public $userSession;
+
+    public function __construct(){
+        parent::__construct();
+        $this->userSession = new UserSesion();
+    }
+
+    public function render(){
+        $rol_usuario = $this->userSession->getRole();
+    
+        //Verifica si el usuario es un candidato y muestra la vista
+        if ($rol_usuario === 1) { 
+            $this->view->tituloPage = "Perfil de usuario";
+            $this->view->render('perfilUsuario/index');
+        } else {
+            header('Location: ' . constant('URL'));
+            exit(); 
+        }
+    }
+}
+
+?>
---
+++ bolsa-trabajo/views/perfilUsuario/index.php
@@ -0,0 +1,11 @@
+<?php require 'views/partials/header.php';?>
+
+    <?php
+        require_once __DIR__ .'../../../controllers/userSesion.php';
+        $userSession = new UserSesion();
+        $currentUser = $userSession->getCurrentUser();
+
+        echo $currentUser['user'];
+    ?>
+
+<?php require 'views/partials/footer.php'?>

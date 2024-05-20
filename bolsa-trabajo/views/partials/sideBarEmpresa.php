7bdbc79ee5c4574e51a149dc93bdf7f807898e08
DiosTeOdia <art3611@gmail.com>
2024-05-20 17:13:11 -0600

    vista registrar datos empresa

---
+++ bolsa-trabajo/views/partials/sideBarEmpresa.php
@@ -0,0 +1,8 @@
+<aside class='br-1 border max-w-80 h-screen flex flex-col justify-start
+items-center'>
+    <div class='flex flex-col gap-y-5 p-10'>
+        <a href='' class='font-bold'>Informacion de la empresa</a>
+        <a href='' class='font-bold'>Ofertas aplicadas</a>
+        <a href='' class='font-bold'>Historial de solicitudes</a>
+    </div>
+</aside>
--- bolsa-trabajo/views/perfilEmpresa/index.php
+++ bolsa-trabajo/views/perfilEmpresa/index.php
@@ -1,10 +1,40 @@
-<?php require 'views/partials/header.php';?>
-    <?php
-        require_once __DIR__ .'../../../controllers/userSesion.php';
-        $userSession = new UserSesion();
-        $currentUser = $userSession->getCurrentUser();
+<?php
+ require 'views/partials/header.php';
+ 
+?>
+<main class='flex'>
+<?php require 'views/partials/sideBarEmpresa.php';?>
 
-        echo $currentUser['user'];
-    ?>
+<section class='flex justify-start ml-10 items-start p-10 w-full'>
+<?php
+    require_once __DIR__ .'../../../controllers/userSesion.php';
+    $userSession = new UserSesion();
+    $currentUser = $userSession->getCurrentUser();
+
+    if(!$currentUser['profile']){
+       echo "Aun no tienes has llenado tus datos de perfil";
+    }
+
+    echo '<div>
+            <div class="flex items-center gap-5">
+            <img src='. $currentUser['profile'] . ' height="200" width="200 alt="Perfil de la empresa" />
+                <h1 class="text-xl font-bold">' . $currentUser['user'] . '</h1>
+            </div>
+            <h2 class="font-bold text-xl"> Descripcion de la empresa</h2>
+            <p>' . $currentUser['descripcion'] . '</p>
+          </div>';
+?>
+</section>
+<div class='bg-[#F3F6FB] md:w-[725px] md:h-[550px] p-10 rounded-lg'>
+<form action="" class='flex flex-col'>
+    <label for="descripcion">Agregar una descripcion <span class='text-red-500'>*</span></label>   
+    <input type="text"  class="h-10 md:h-16 rounded-lg">
+    <label for="perfil">Agrega una imagen de perfil  <span class='text-red-500'>*</span></label></label>
+    <input type="text">
+</form>
+</div>
+
+</main>
+
 
 <?php require 'views/partials/footer.php'?>

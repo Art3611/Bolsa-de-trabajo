9434f43c90667e4ccff6cc78c31adf25201cae33
DiosTeOdia <art3611@gmail.com>
2024-05-27 21:22:45 -0600

    ajuste de caracteres minimos

--- bolsa-trabajo/views/perfilEmpresa/publicarOferta.php
+++ bolsa-trabajo/views/perfilEmpresa/publicarOferta.php
@@ -14,22 +14,22 @@
     <input type="text" name='nombre_trabajo' class=' h-10 md:h-16 rounded-lg' minlength="10"  maxlength="255" required>
 
     <label for="descripcion">Descripcion del empleo <span class='text-red-500'>*</span></label>
-    <input type="text" name='descripcion' class=' h-10 md:h-16 rounded-lg' maxlength="560" minlength="50" required>
+    <input type="text" name='descripcion' class=' h-10 md:h-16 rounded-lg' maxlength="560" minlength="20" required>
 
     <label for="ubicacion">Ubicacion del empleo <span class='text-red-500'>*</span></label>
-    <input type="text" name='ubicacion' class=' h-10 md:h-16 rounded-lg' minlength="50"  maxlength="255" required>
+    <input type="text" name='ubicacion' class=' h-10 md:h-16 rounded-lg' minlength="10"  maxlength="255" required>
 
     <label for="contrato">Tipo de contrato <span class='text-red-500'>*</span></label>
-    <input type="text" name='contrato' class=' h-10 md:h-16 rounded-lg' minlength="50"  maxlength="255" required>
+    <input type="text" name='contrato' class=' h-10 md:h-16 rounded-lg' minlength="5"  maxlength="255" required>
 
     <label for="salario">Salario <span class='text-red-500'>*</span></label>
-    <input type="text" name='salario' class=' h-10 md:h-16 rounded-lg' minlength="50"  maxlength="255" required>
+    <input type="text" name='salario' class=' h-10 md:h-16 rounded-lg' minlength="5"  maxlength="255" required>
 
     <label for="duracion">Duracion <span class='text-red-500'>*</span></label>
-    <input type="text" name='duracion' class=' h-10 md:h-16 rounded-lg' minlength="50"  maxlength="255" required>
+    <input type="text" name='duracion' class=' h-10 md:h-16 rounded-lg' minlength="5"  maxlength="255" required>
 
     <label for="requisitos">Requisitos <span class='text-red-500'>*</span></label>
-    <textarea type="text" name='requisitos' class='h-10 md:h-16 rounded-lg resize-none'minlength="50"  maxlength="255" required></textarea>
+    <textarea type="text" name='requisitos' class='h-10 md:h-16 rounded-lg resize-none'minlength="15"  maxlength="255" required></textarea>
 
     <button class='p-5 rounded-lg text-white bg-blue-500 mt-10'>Publicar oferta</button>
 
